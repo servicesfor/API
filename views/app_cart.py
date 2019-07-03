423d1977d351ae9d81fa7395b39f6ffbf46499f2
baiyonghu <baiyonghu8@163.com>
2019-07-03 09:10:44 +0800

    'commit_cart'

--- views/app_cart.py
+++ views/app_cart.py
@@ -10,15 +10,15 @@ blue = Blueprint("cart_api",__name__)
 def add_cart():
     req_data = request.get_json()
 
-    # if not req_data['token']:
-    #     return jsonify({
-    #         "code": 400,
-    #         "msg": "您还未登录,请先登录!"
-    #     })
-    # user_id = get_token_user_id(req_data['token'])  # 通过token获取id
-    # med_id = req_data['med_id']
-    user_id = 4 # 通过token获取id
-    med_id = 9793
+    if not req_data['token']:
+        return jsonify({
+            "code": 400,
+            "msg": "您还未登录,请先登录!"
+        })
+    user_id = get_token_user_id(req_data['token'])  # 通过token获取id
+    med_id = req_data['med_id']
+    # user_id = 4 # 通过token获取id
+    # med_id = 9793
 
     dao = CartDao()
     data = dao.add_carts(user_id,med_id)       #执行添加购物车函数
@@ -31,17 +31,17 @@ def add_cart():
 
 @blue.route('/sub_cart/', methods=('GET',))     #删除购物车接口
 def sub_cart():
-    # req_data = request.get_json()
-    #
-    # if not req_data['token']:
-    #     return jsonify({
-    #         "code": 400,
-    #         "msg": "您还未登录,请先登录!"
-    #     })
-    # user_id = get_token_user_id(req_data['token'])  # 通过token获取id
-    # med_id = req_data['med_id']
-    user_id = 4  # 通过token获取id
-    med_id = 9792
+    req_data = request.get_json()
+
+    if not req_data['token']:
+        return jsonify({
+            "code": 400,
+            "msg": "您还未登录,请先登录!"
+        })
+    user_id = get_token_user_id(req_data['token'])  # 通过token获取id
+    med_id = req_data['med_id']
+    # user_id = 4  # 通过token获取id
+    # med_id = 9792
     dao = CartDao()
     data = dao.sub_carts(user_id,med_id)  # 执行删除购物车函数
     return jsonify({
